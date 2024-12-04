34f7109ef5c88d4e28dcc7f8c388cb216fbee597
Korra-5 <dani05corral@gmail.com>
2024-12-04 17:12:10 +0100

    Primer commit

---
+++ Scripts/Espada.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Espada : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public void _on_body_entered (Node2D body){
+		GD.Print("Añadido");
+		Boolean sword=true;
+		QueueFree();
+		GD.Print(sword);
+		
+		GetTree().ChangeSceneToFile("res://Scenes/Fin.tscn");
+
+		
+	}
+	
+}
